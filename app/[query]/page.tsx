94b8a5c807ef9c33e1be6740060fdb180e5c970a
adzashel <seigneur.noir15@gmail.com>
2025-03-12 21:45:51 +0700

    create navbar and routing pages

---
+++ app/[query]/page.tsx
@@ -0,0 +1,11 @@
+ const page = () => {
+    return (
+    <div>
+        <h1>Hello, Next.js!</h1>
+        <p>Welcome to your new Next.js app.</p>
+        <p>Learn more about Next.js on the <a href="https://nextjs.org/">official Next.js documentation site</a>.</p>
+    </div>
+    )
+}
+
+export default page
